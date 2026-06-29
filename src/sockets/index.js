3c0170649b5d424fdd2938c1b9fb9fc1590cc4f0
Aadi639707 <aadi639707@gmail.com>
2026-06-29 19:54:25 +0530

    Update index.js

--- src/sockets/index.js
+++ src/sockets/index.js
@@ -1,1 +1,10 @@
+const matchmaking = require('./matchmaking');
+const gameHandlers = require('./gameHandlers');
 
+module.exports = (io) => {
+  io.on('connection', (socket) => {
+    console.log('socket connected', socket.id);
+    matchmaking(io, socket);
+    gameHandlers(io, socket);
+  });
+};
